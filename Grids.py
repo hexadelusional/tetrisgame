adfbbfea9fdbddf4b2d6025ed77d8420f384bfa4
eleda16 <117863030+eleda16@users.noreply.github.com>
2022-11-09 22:09:27 +0100

    Update Grids.py

--- Grids.py
+++ Grids.py
@@ -6,18 +6,34 @@ def grid():
 
 
 
-#Diamond
+#Diamond grid
 
-#Triangle
+def grid_diamond():
+    length = 12
+    print("    a " + " b " + " c " + " d " + " e " + " f " + " g " + " h " + " i " + " j " + " k " + " l " + " m " + " n " + " o " + " p " + " q " + " r " + " s " + " t " + " u " + " v " + " w " + " x " + " y ")
+    print(" ╔" + "═" * 77 + "╗")
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for i in range(length):
+        print(alphabet[i]+"║  " +"0  " * (length - i)+ "1  " * (2 * i + 1)+ "0  " * (length - i) + "║")
+    for i in range(length - 2, -1, -1):
+        print(alphabet[i]+"║  " +"0  " * (length - i)+ "1  " * (2 * i + 1)+ "0  " * (length - i) + "║")
+    print(" ╚" + "═" * 77 + "╝")
+print(grid_diamond())
+
+#Triangle grid
+
+def grid_triangle():
+    length = 12
+    print("    a " +" b "+" c "+ " d "+" e "+" f "+" g "+ " h "+" i "+" j "+" k "+" l "+" m "+" n "+" o "+" p "+" q "+" r "+" s "+" t "+" u "+" v "+" w "+" x "+" y ")
+    print(" ╔"+"═"*77+"╗")
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for i in range(length):
+        print(alphabet[i]+"║  " + "0  " * (length - i)+ "1  " * (2 * i + 1)+"0  " * (length - i) + "║")
+    print(" ╚" + "═" * 77 + "╝")
+print(grid_triangle())
 
 
-#Circle
-
-
-
-
-
-
+#Circle grid
 
 
 
