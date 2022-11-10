9b3fdb027c5d035e6ff8757265f9cf5b8ec8d732
eleda16 <117863030+eleda16@users.noreply.github.com>
2022-11-10 09:26:06 +0100

    Update Grids.py
    j'ai fais la fonction qui lis la grid selon l'entrée, elle est tout en bas du code.
    -> il manque la grid du cercle qu'on avait dit que tu faisait

--- Grids.py
+++ Grids.py
@@ -1,7 +1,3 @@
-def grid():
-    answers = ["Circle", "Triangle", "Diamond"]
-    format = int(input("What type of board do you want to play on ?"))
-    file = open((format + ".txt"), "r")
 
 
 # DIAMOND GRID
@@ -16,10 +12,6 @@ def grid_diamond():
     for i in range(length - 2, -1, -1):
         D.write("0  " * (length - i) + "1  " * (2 * i + 1) + "0  " * (length - i) + "\n")
     D.close()
-    # reading the lines of the D txt for retranscription in ascii code
-    with open("diamondgrid.txt") as d:
-        for line in d:
-            print(line.strip())
 grid_diamond()
 
 """ 
@@ -46,12 +38,7 @@ def grid_triangle():
     for i in range(length):
         T.write("0  " * (length - i)+ "1  " * (2 * i + 1)+"0  " * (length - i)+"\n")
     T.close()
-    # reading the lines of the T txt for retranscription in ascii code
-    with open("trianglegrid.txt") as t:
-        for line in t:
-            print(line.strip())
 grid_triangle()
-
 """ 
 def grid_triangle():
     length = 12
@@ -68,3 +55,22 @@ print(grid_triangle())
 
 
 # CIRCLE GRID
+
+
+
+
+def grid():
+    format = input("What board do you want to play on ? [Circle] [Triangle] [Diamond] ")
+    while format!="diamond" and format!="triangle" and format!="circle":
+        format = input("Error, this board does not exist. You must write the name in lowercase letters of one of the boards proposed: [Circle] [Triangle] [Diamond] ")
+    if format == "diamond":
+        with open("diamondgrid.txt") as d:
+            for line in d:
+                print(line.strip())
+    if format == "triangle":
+        with open("trianglegrid.txt") as t:
+            for line in t:
+                print(line.strip())
+
+
+grid()
