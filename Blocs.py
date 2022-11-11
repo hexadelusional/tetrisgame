2c7e979d2528489914babdb78abb6ee16baf04ab
eleda16 <117863030+eleda16@users.noreply.github.com>
2022-11-11 18:50:30 +0100

    Update Blocs.py

--- Blocs.py
+++ Blocs.py
@@ -1,34 +1,34 @@
+################################################################ BLOCKS ################################################################
+
+
+from Grid import*
 import random
 
-#Creation of all the blocks
-#Espace de width à droite
-#Espace de length en haut
+################ COMMON BLOCKS ################
 
-################ COMMON BLOCS ################
+common_blocks = [[] for i in range(20)]
+common_blocks[0] = [[0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0], [1, 1, 0, 0]]
+common_blocks[1] = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 0, 0], [1, 1, 0, 0]]
+common_blocks[2] = [[0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0], [1, 1, 1, 0]]
+common_blocks[3] = [[0, 0, 0, 0], [1, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]]
+common_blocks[4] = [[0, 0, 0, 0], [1, 0, 0, 0], [1, 1, 0, 0], [1, 0, 0, 0]]
+common_blocks[5] = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 0, 0], [1, 1, 1, 0]]
+common_blocks[6] = [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 0, 0], [0, 1, 1, 0]]
+common_blocks[7] = [[0, 0, 0, 0], [1, 0, 0, 0], [1, 1, 0, 0], [0, 1, 0, 0]]
+common_blocks[8] = [[1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0]]
+common_blocks[9] = [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 0, 0], [1, 1, 0, 0]]
+common_blocks[10] = [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 0, 0], [0, 1, 0, 0]]
+common_blocks[11] = [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 0, 0], [1, 0, 0, 0]]
+common_blocks[12] = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0], [1, 1, 1, 0]]
+common_blocks[13] = [[0, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [1, 1, 0, 0]]
+common_blocks[14] = [[0, 0, 0, 0], [0, 1, 0, 0], [1, 1, 0, 0], [0, 1, 0, 0]]
+common_blocks[15] = [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 0], [0, 1, 0, 0]]
+common_blocks[16] = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 1, 0], [1, 1, 0, 0]]
+common_blocks[17] = [[0, 0, 0, 0], [0, 1, 0, 0], [1, 1, 0, 0], [1, 0, 0, 0]]
+common_blocks[18] = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0]]
+common_blocks[19] = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1]]
 
-common_blocs = [[] for i in range(20)]
-common_blocs[0] = [[0,0,0,0],[0,0,0,0],[1,0,0,0],[1,1,0,0]]
-common_blocs[1] = [[0,0,0,0],[0,0,0,0],[0,1,0,0],[1,1,0,0]]
-common_blocs[2] = [[0,0,0,0],[0,0,0,0],[1,0,0,0],[1,1,1,0]]
-common_blocs[3] = [[0,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,0,0]]
-common_blocs[4] = [[0,0,0,0],[1,0,0,0],[1,1,0,0],[1,0,0,0]]
-common_blocs[5] = [[0,0,0,0],[0,0,0,0],[0,1,0,0],[1,1,1,0]]
-common_blocs[6] = [[0,0,0,0],[0,0,0,0],[1,1,0,0],[0,1,1,0]]
-common_blocs[7] = [[0,0,0,0],[1,0,0,0],[1,1,0,0],[0,1,0,0]]
-common_blocs[8] = [[1,0,0,0],[1,0,0,0],[1,0,0,0],[1,0,0,0]]
-common_blocs[9] = [[0,0,0,0],[0,0,0,0],[1,1,0,0],[1,1,0,0]]
-common_blocs[10] = [[0,0,0,0],[0,0,0,0],[1,1,0,0],[0,1,0,0]]
-common_blocs[11] = [[0,0,0,0],[0,0,0,0],[1,1,0,0],[1,0,0,0]]
-common_blocs[12] = [[0,0,0,0],[0,0,0,0],[0,0,1,0],[1,1,1,0]]
-common_blocs[13] = [[0,0,0,0],[1,0,0,0],[1,0,0,0],[1,1,0,0]]
-common_blocs[14] = [[0,0,0,0],[0,1,0,0],[1,1,0,0],[0,1,0,0]]
-common_blocs[15] = [[0,0,0,0],[0,0,0,0],[1,1,1,0],[0,1,0,0]]
-common_blocs[16] = [[0,0,0,0],[0,0,0,0],[0,1,1,0],[1,1,0,0]]
-common_blocs[17] = [[0,0,0,0],[0,1,0,0],[1,1,0,0],[1,0,0,0]]
-common_blocs[18] = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[1,0,0,0]]
-common_blocs[19] = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[1,1,1,1]]
-
-################ CIRCLE BLOCS ################
+################ CIRCLE BLOCKS ################
 
 circle_list = [[] for i in range(12)]
 circle_list[0] = [[0,0,0,0,0],[1,1,1,1,0],[1,1,1,1,0],[1,1,1,1,0],[1,1,1,1,0]]
@@ -44,7 +44,7 @@ circle_list[9] = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,1,1,1,1],[1,0,0,0,1]]
 circle_list[10] = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,1,1,1,1]]
 circle_list[11] = [[0,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,1,0],[1,1,1,1,0]]
 
-################ DIAMOND BLOCS ################
+################ DIAMOND BLOCKS ################
 
 diamond_list = [[] for i in range(14)]
 diamond_list[0] = [[0,0,0,0,0],[0,0,1,1,0],[0,1,1,0,0],[1,1,0,0,0],[1,0,0,0,0]]
@@ -62,7 +62,7 @@ diamond_list[11] = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,1,1,1,0],[0,0,0,1,0]]
 diamond_list[12] = [[0,0,0,0,0],[1,1,0,0,0],[0,1,0,0,0],[0,1,0,0,0],[0,1,0,0,0]]
 diamond_list[13] = [[0,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[1,1,0,0,0]]
 
-################ TRIANGLE BLOCS ################
+################ TRIANGLE BLOCKS ################
 
 triangle_list = [[] for i in range(11)]
 triangle_list[0] = [[1,0,0],[1,1,1],[0,0,1]]
@@ -77,69 +77,90 @@ triangle_list[8] = [[0,0,0],[1,0,0],[1,0,0]]
 triangle_list[9] = [[0,1,0],[1,1,1],[0,1,0]]
 triangle_list[10] = [[0,0,0],[0,0,0],[1,1,0]]
 
-blocs_list = [diamond_list, triangle_list, circle_list]
+blocks_list = [diamond_list, triangle_list, circle_list]
 
 
 
 # function print_blocs(grid) which takes as parameters the shape of the chosen tray, and which displays the list of all the blocks associated with it
 
-def print_blocs(grid):
-
-    if grid == "triangle.txt":
-        for i in range(len(blocs_list[1])):
-            for j in range(len(blocs_list[1][0])):
-                for k in range(len(blocs_list[1][0][j])):
-                    if blocs_list[1][i][j][k] == 0:
-                        print(chr(10240), end="  ")
-                    elif blocs_list[1][i][j][k] == 1:
-                        print(chr(9632), end="  ")
-                print("\n".strip())
-            print("_")
-
-    elif grid == "diamond.txt":
-        for i in range(len(blocs_list[0])):
-            for j in range(len(blocs_list[0][0])):
-                for k in range(len(blocs_list[0][0][j])):
-                    if blocs_list[0][i][j][k] == 0:
-                        print(chr(10240), end="  ")
-                    elif blocs_list[0][i][j][k] == 1:
-                        print(chr(9632), end="  ")
-                print("\n".strip())
-            print("_")
-
-    elif grid == "circle.txt":
-        for i in range(len(blocs_list[2])):
-            for j in range(len(blocs_list[2][0])):
-                for k in range(len(blocs_list[2][0][j])):
-                    if blocs_list[2][i][j][k] == 0:
-                        print(chr(10240), end="  ")
-                    elif blocs_list[2][i][j][k] == 1:
-                        print(chr(9632), end="  ")
-                print("\n".strip())
-            print("_")
-            
+def print_blocks(grid):
     print("COMMON BLOCS:")
-    for i in range(len(common_blocs)):
-        for j in range(len(common_blocs[i])):
-            for k in range(len(common_blocs[i][j])):
-                if common_blocs[i][j][k] == 0:
+    for i in range(len(common_blocks)):
+        for j in range(len(common_blocks[i])):
+            for k in range(len(common_blocks[i][j])):
+                if common_blocks[i][j][k] == 0:
                     print(chr(10240), end="  ")
-                elif common_blocs[i][j][k] == 1:
+                elif common_blocks[i][j][k] == 1:
                     print(chr(9632), end="  ")
             print("\n".strip())
         print("_")
 
+    if grid == "diamond.txt":
+        A = 0
+        print("DIAMOND BLOCS:")
+        for i in range(len(blocks_list[A])):
+            for j in range(len(blocks_list[A][0])):
+                for k in range(len(blocks_list[A][0][j])):
+                    if blocks_list[0][i][j][k] == 0:
+                        print(chr(10240), end="  ")
+                    elif blocks_list[0][i][j][k] == 1:
+                        print(chr(9632), end="  ")
+                print("\n".strip())
+            print("_")
+        return A
+    elif grid == "triangle.txt":
+        A = 1
+        print("TRIANGLE BLOCS:")
+        for i in range(len(blocks_list[A])):
+            for j in range(len(blocks_list[A][0])):
+                for k in range(len(blocks_list[A][0][j])):
+                    if blocks_list[A][i][j][k] == 0:
+                        print(chr(10240), end="  ")
+                    elif blocks_list[1][i][j][k] == 1:
+                        print(chr(9632), end="  ")
+                print("\n".strip())
+            print("_")
+        return A
 
-current_blocs = print_blocs("circle.txt")
+    elif grid == "circle.txt":
+        A = 2
+        print("CIRCLE BLOCS:")
+        for i in range(len(blocks_list[A])):
+            for j in range(len(blocks_list[A][0])):
+                for k in range(len(blocks_list[A][0][j])):
+                    if blocks_list[2][i][j][k] == 0:
+                        print(chr(10240), end="  ")
+                    elif blocks_list[2][i][j][k] == 1:
+                        print(chr(9632), end="  ")
+                print("\n".strip())
+            print("_")
+        return A
 
 
-def select_bloc():
-    # selection of blocs following policy 1
+current_blocks = print_blocks(current_grid + ".txt")
+
+
+def select_block():
+    question = int(input("policy 1 or policy 2 ? (enter 1 or 2)"))
+    if question == 1:
+        # selection of blocs following policy 1 : display at each turn of the game all the available blocks and the user selects one
+        print(blocks_list[current_blocks])
+
+
+    elif question == 2:
+        # selection of blocs following policy 2 : display only 3 randomly selected blocks
+        the_random_three = random.sample(blocks_list[current_blocks], 3)
+        print("RANDOM BLOC PIC: ")
+        for i in range(len(the_random_three)):
+            for j in range(len(the_random_three[i])):
+                for k in range(len(the_random_three[i][j])):
+                    if the_random_three[i][j][k] == 0:
+                        print(chr(10240), end="  ")
+                    elif the_random_three[i][j][k] == 1:
+                        print(chr(9632), end="  ")
+                print("\n".strip())
+            print("_")
 
 
 
-    # selection of blocs following policy 2
-    print(random.sample(blocs_list[1],3))
-
-
-select_bloc()
+select_block()
