4d459a4f39e1c77b3abf5148279c493eb047ac90
eleda16 <117863030+eleda16@users.noreply.github.com>
2022-11-23 09:21:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
-print("######################################### WELCOME ######################################### \n")
-ready = input("-> Press [enter] to start playing ! ")
-while ready == " ":
-    ready = input("-> Press [enter] to start playing ! ")
+if __name__ == "__main__":
+    print("######################################### WELCOME ######################################### \n")
 
-from Blocks import*
+    ready = input("-> Press [ENTER] to start playing ! ")
+    while ready == " ":
+        ready = input("-> Press [enter] to start playing ! ")
+    from Blocks import *
+
