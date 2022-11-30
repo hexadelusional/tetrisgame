885f5368fb4dde937e87d99a6c71aee83f5b8804
eleda16 <117863030+eleda16@users.noreply.github.com>
2022-11-30 13:37:07 +0100

    Update Blocks.py

--- Blocks.py
+++ Blocks.py
@@ -1,6 +1,4 @@
-from Grids import*
-import random
-
+from main import*
 ################################################################ BLOCKS ################################################################
 
 
@@ -83,29 +81,23 @@ blocks_list = [diamond_list, triangle_list, circle_list]
 
 # function print_blocs(grid) which takes as parameters the shape of the chosen tray, and which displays the list of all the blocks associated with it
 
-def print_blocks(grid):
-    if grid == 'circle.txt' :
-        current_blocks = common_blocks + circle_list
-    elif grid == 'diamond.txt' :
-        current_blocks = common_blocks + diamond_list
-    else :
-        current_blocks = common_blocks + triangle_list
+def print_blocks(blocks):
     print("ALL BLOCKS :\n")
-    if len(current_blocks) % 5 == 0 :
-        length = len(current_blocks)
+    if len(blocks) % 5 == 0 :
+        length = len(blocks)
     else :
-        length = len(current_blocks) - (len(current_blocks)%5 + 1)
+        length = len(blocks) - (len(blocks) % 5 + 1)
     for block in range(0,length,5):
         #Printing the line with name of blocks
         for n in range(block, block+5) :
             print('| {:^10} |'.format("Block "+ str(n)),end= "")
         print("\n".strip())
-        max_rows = max(len(current_blocks[0]),len(current_blocks[-1]))
+        max_rows = max(len(blocks[0]), len(blocks[-1]))
         for row in range(max_rows):
             for block_i in range(block,block+5):
-                if row < len(current_blocks[block_i]) :
+                if row < len(blocks[block_i]) :
                     substring = ""
-                    for elt in current_blocks[block_i][row]:
+                    for elt in blocks[block_i][row]:
                         if elt == 0 :
                             substring += chr(10240) + " "
                         else :
@@ -115,14 +107,14 @@ def print_blocks(grid):
                     print('| {:^10} |'.format(" "),end="")
             print("\n".strip())
         print('_' * 70)
-    if length != len(current_blocks) :
-        for block in range(length+1,len(current_blocks)):
+    if length != len(blocks) :
+        for block in range(length+1, len(blocks)):
             print('| {:^10} |'.format("Block " + str(block)), end="")
         print("\n".strip())
-        for row in range(len(current_blocks[-1])):
-            for block in range(length + 1, len(current_blocks)):
+        for row in range(len(blocks[-1])):
+            for block in range(length + 1, len(blocks)):
                 substring = ""
-                for elt in current_blocks[block][row]:
+                for elt in blocks[block][row]:
                     if elt == 0 :
                         substring += chr(10240) + " "
                     else :
@@ -130,3 +122,57 @@ def print_blocks(grid):
                 print('| {:^10} |'.format(substring),end="")
             print("\n".strip())
         print('_' * 70)
+
+
+
+def select_blocks():
+    for i in range(len(current_blocks)):
+        chosen_block = int(input("Enter the next block to place on the grid: "))
+        while chosen_block < 0 or chosen_block > len(current_blocks):
+            chosen_block = int(input("Enter the next block to place on the grid (you have to enter the number next to the block you want) : "))
+    return current_blocks[chosen_block]
+
+
+
+"""
+Matrix = []
+def valid_position(grid, row, col):
+    x = 0
+    z = 0
+    maju = "ABCDEFGHIJKLMNOPQRSTUVWXY"
+    minu = "abcdefghijklmnopqrstuvwxy"
+    for i in range(25):
+        if row == maju[i]:
+            x = i
+        if col == minu[i]:
+            y = i
+
+    # filling a matrix with the elements of the grid file
+    with open(grid + ".txt", "r") as G:
+        for line in G:
+            l = []
+            line = line.strip()
+            columns = line.split()
+            for elt in columns:
+                l.append(elt)
+            Matrix.append(l)
+
+    # going through the matrix to check if the block is positionable
+    possible = True
+    while possible == True:
+        for i in range(len(select_blocks())-1):
+            for j in range(len(select_blocks()[i])):
+                if Matrix[x][y]==0:
+                    if select_blocks()[-1][0] == 1:
+                        print("Error, you made a mistake ! ")
+                        possible = False
+                    if select_blocks()[]
+                if Matrix[x][y]==1:
+                    if select_blocks()[-1][0] == 1:
+                    if select_blocks()[i][0] == 1 and Matrix[x][y-1]
+            if select_blocks()[i][0] ==
+
+print(valid_position(current_grid, "A", "b"))
+
+
+"""
