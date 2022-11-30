6e67b350b47933ffd9f18493e34f8d7b7e1bf774
eleda16 <117863030+eleda16@users.noreply.github.com>
2022-11-30 13:36:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Grid import*
 from Blocks import*
-
+import random
 
 if __name__ == "__main__":
     print("######################################### WELCOME ######################################### \n")
@@ -14,13 +14,6 @@ if __name__ == "__main__":
     while current_grid != "diamond" and current_grid != "triangle" and current_grid != "circle":
          current_grid = input("Error, this board shape does not exist. You must write the name (in lowercase letters) of one of the board shapes proposed; [Circle] [Triangle] [Diamond]: ")
 
-    # initializing current_blocks: the blocks that the user is allowed to use according to the grid
-    if current_grid == "diamond":
-        current_blocks = common_blocks + diamond_list
-    elif current_grid == "triangle":
-        current_blocks = common_blocks + triangle_list
-    else:
-        current_blocks = common_blocks + diamond_list
 
     # asking what board shape the user wants
     current_size = input("What board size do you want to play on ? [S] [M] [L] ")
@@ -37,12 +30,17 @@ if __name__ == "__main__":
     # saving the grid in a new file called 'game_grid.txt'
     save_grid("game_grid.txt", current_grid)
 
-
     # printing the current grid in the console
-    M=print_grid(current_grid)
+    print_grid(current_grid)
 
+    if current_grid == 'circle.txt' :
+        current_blocks = common_blocks + circle_list
+    elif current_grid == 'diamond.txt' :
+        current_blocks = common_blocks + diamond_list
+    else :
+        current_blocks = common_blocks + triangle_list
     # printing the blocks that the user can use according to the grid he chose
-    print_blocks((current_grid + ".txt"))
+    print_blocks(current_blocks)
 
     # asking which policy the user wants
     question = input("policy 1 or policy 2 ? (enter 1 or 2) ")
@@ -51,13 +49,14 @@ if __name__ == "__main__":
     print("THESE ARE THE AVAILABLE BLOCKS: ")
 
 
+
     if question == "1": # selection of blocks following policy 1 (to display at each turn of the game all the available blocks and the user selects one)
         print_blocks((current_grid + ".txt"))
 
     elif question == "2": # selection of blocks following policy 2 (to display only 3 randomly selected blocks)
 
         # choosing 3 random blocks that correspond  to the chosen grid using the 'index'
-        the_random_three = random.sample(current_blocks, 3)
+        the_random_three = random.sample(blocks, 3)
 
         # printing the 3 random blocks
         print("RANDOM BLOC PIC: ")
@@ -70,7 +69,6 @@ if __name__ == "__main__":
                         print(chr(9632), end="  ")
                 print("\n".strip())
             print("_")
-
 
 
     # checking if the coordinates entered are admissible according to the size of the grid
@@ -87,6 +85,6 @@ if __name__ == "__main__":
             "Enter the x coordinates (you must enter the letter (in lowercase) corresponding to the column you want): "))
 
     y = str(input("Enter the y coordinates: "))
-    while (y < chr(65) or y > (chr(65 + length - 1)) or len(y) != 1):
+    while y < chr(65) or y > (chr(65 + length - 1)) or len(y) != 1:
         y = str(input(
             "Enter the y coordinates (you must enter the letter (in uppercase) corresponding to the row you want): "))
