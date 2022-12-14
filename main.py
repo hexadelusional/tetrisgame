8f98eec5e4c12ee49a1babfe2c9d876609702a8d
eleda16 <117863030+eleda16@users.noreply.github.com>
2022-12-14 08:38:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     print('{: ^150}\n'.format(" Wanna play Tetris ?"))
 
     # start of the game
-    ready = input('{: ^150}'.format(" Press [ENTER] to start a start playing ;)\n \n"))
+    ready = input('{: ^150}'.format(" -> Press [ENTER] to start a start playing ;)\n \n"))
 
     # asking the user if he wants to resume a saved game or if he wants to start a brand new game
     print('{: ^145}\n'.format(" -> Press [R] to resume a previously saved game !"))
@@ -17,10 +17,15 @@ if __name__ == "__main__":
     while option != "R" and option != "r" and option != "N"and option != "n":
          option = input("\n -> Error, press letter [R] to resume OR press letter [N] for a new game : ")
 
-    if option == "R" or option == "r":
-        path = input("\n Give the name of the file you want to load : ")
-        while len(path) >= 10:
-            path = input("\n Give the name of the file you want to load, it's the name of the previous grid you played on (without the.txt) : ")
+    if option == "R" or option == "r":  # if the user chose to resume a previous game
+        path = input("\n Give the name of the file you want to load : ")  # ask the name of the file to open
+        file_exists = False  # assuming the file doesn't exist
+        while file_exists == False:  # while the name of the file given by the user doesn't exist
+            try:  # test if the file can be open
+                f = open(path + ".txt")
+                file_exists = True  # if it can, the file exists
+            except IOError:  # if the opening of the file returns an error (here FileNotFoundError), keep asking for a correct file name
+                path = input("\n -> Error, this file doesn't exist. Give the name of the file you want to load, the one of the previous grid you played on (without the.txt) : ")
         length, score, current_grid = reloading_all_data(path)
 
     else:
@@ -49,6 +54,8 @@ if __name__ == "__main__":
         else:
             length = 21
 
+        score = 0
+
 
     # reading the grid chosen above according to its size 'current_size'
     M = read_grid(current_grid)
@@ -69,8 +76,6 @@ if __name__ == "__main__":
         question = input("\n -> Error, this policy does not exist. You must enter 1 for [Policy 1] or 2 for [Policy 2] : ")
 
 
-
-
 # Start of while loop (while the player wants to play // while rep == "Y")
         # We select block
         # Ask for coordinates
@@ -79,7 +84,6 @@ if __name__ == "__main__":
         # print the current score
         # ask the user if he wants to continue playing (rep = input("Y" = yes, "N" = No)
     answer = "Y"
-    score = 0
     while answer == "Y":
         # showing the user the blocks he can use
         print("\n -> The available blocks are:")
@@ -92,7 +96,7 @@ if __name__ == "__main__":
         # reading and displaying the current grid
         M = read_grid(current_grid)
         print_grid(M)
-        print('{: ^150}\n'.format(" SCORE = ", score))
+        print('{: ^140}\n'.format(" SCORE = " + str(score)))
 
         # printing the block the user just chosed
         print_blocks([chosen_block])
@@ -131,20 +135,20 @@ if __name__ == "__main__":
                  y += 1
 
             # asking the player if they want to pursue the game or quit
-            answer = input("\n Do you wish to continue ? Y or N : ")
+            answer = input("\n -> Do you wish to continue ? Y or N : ")
             while answer != "Y" and answer != "N":
-                answer = input("\n Do you wish to continue ? Please type 'Y' or 'N' : ")
+                answer = input("\n -> Do you wish to continue ? Please type 'Y' or 'N' : ")
 
     # asking the user if he wants to save his game in a file, so that he can pick up from where he left off in the future
-    wanna_save = input("\n Do you want to save your game to resume another time ? Y or N : ")
+    wanna_save = input("\n -> Do you want to save your game to resume another time ? Y or N : ")
     while wanna_save != "Y" and wanna_save!= "N":
-        wanna_save = input("\n Do you want to save your game to resume another time ? Please type 'Y' or 'N' : ")
+        wanna_save = input("\n -> Do you want to save your game to resume another time ? Please type 'Y' or 'N' : ")
 
     if wanna_save == "Y":
         # saving the current grid in a file named by the user
-        name = input("\n Give the name of the file you want to save your grid in : ")
+        name = input("\n -> Give the name of the file you want to save your grid in : ")
         while len(name) >= 10:
-            name = input("\n Give the name of the file you want to save your grid in, the name cannot exceed 10 characters : ")
+            name = input("\n -> Give the name of the file you want to save your grid in, the name cannot exceed 10 characters : ")
         backup_save(name, M, length, score)
 
-    print("\n Your total score was : ", score)
+    print("\n -> Your total score was : ", score)
